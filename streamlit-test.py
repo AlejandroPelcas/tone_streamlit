ff35b950166c16796d7c1f5107b76fc9f4d26b27
Alejandro Pelcastre <alejandro.pelcas@berkeley.edu>
2022-06-20 13:57:41 -0700

    streamlit

---
+++ streamlit-test.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from streamlit_lottie import st_lottie
+import json
+import requests
+
+header = st.container()
+dataset = st.container()
+models = st.container()
+
+with header:
+    st.title("Welcome to our Capstone Project!")
+
+with dataset:
+    st.header("We are using the Sentiment140 dataset with over 1.6 million tweets")
+    st.text("Our  dataset is scrapped from twitter... and its cool! Take a look")
+    data = pd.read_csv("/Users/alejandropelcastre/Documents/capstone/data/text_emotion.csv", encoding = "ISO-8859-1")
+
+    st.write(data.head())
+
+    #train models
+    from sklearn.model_selection import train_test_split
+
+    X = data.content
+    y = data.sentiment
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    # Creating a donut chart of the proportion of each emotion (to provide a representation of the end product):
+    df = data
+    names = list(df['sentiment'].value_counts().index)
+    size = list(df['sentiment'].value_counts().values)
+    size = [i/sum(list(df['sentiment'].value_counts().values)) for i in size]
+
+    my_circle = plt.Circle( (0,0), 0.7, color='white')
+    plt.pie(size, labels=names, colors=['aqua', 'cadetblue', 'royalblue', 'blue',  'cornflowerblue', 'cyan', 'darkslategray', 'deepskyblue', 'dodgerblue', 'lightblue', 'oldlace', 'royalblue'])
+    p = plt.gcf()
+    plt.tight_layout()
+    p.gca().add_artist(my_circle)
+    plt.tight_layout()
+
+    plt.title('Donut Chart of Sentiment Proportions')
+    plt.show()
+    st.pyplot(p)
+
+    #get animations
+    def load_lottiefile(filepath: str):
+        with open(filepath, "r") as f:
+            return json.load(f)
+
+    def load_lottieurl(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    lottie_chart = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_nedzkmew.json")
+
+    st_lottie(
+    lottie_chart,
+    speed=1,
+    reverse=False,
+    loop=True,
+    quality="low", # medium ; high
+    height=300,
+    width=300,
+    key=None,
+    )
